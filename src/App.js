4cb74017d3acbc7e6437038b63e517bdfff3f619
Alwaleed Fares <110339523+waleed00439@users.noreply.github.com>
2022-09-07 00:11:18 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Calculator from './App.css'
+
+export default props => {
+
+    return (
+        <div>
+            <h1 style={{ fontSize: '35pt', marginBottom: '3rem'}}>Calculator</h1>
+            <Calculator />
+        </div>
+    )
+}
---
+++ src/D.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './D.css'
+
+export default props => {
+    
+    return (
+        <div className='display'>{props.value}</div>
+    )
+}
